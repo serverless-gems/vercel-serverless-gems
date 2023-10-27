7ba1251b52576e249ecae94e1ff939ceacf9ea36
Alex Zappa <alex@zappa.dev>
2023-10-27 05:32:44 +0200

    website optimization

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -2,6 +2,7 @@ module.exports = {
 	title: "Serverless Gems for Vercel",
   suffix: " | Serverless Gems for Vercel",
 	url: "https://serverless-gems.vercel.app/",
+  motherShip: "https://serverless-gems.dev/",
 	language: "en",
 	description: "Serverless Gems is a curated collection of open-source projects for building serverless web applications! 🌐💡 JavaScript-powered and designed for easy deployment, maintenance, and scalability.",
 	author: {
